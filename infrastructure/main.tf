f6e32ba3340571ccfd8930db5935dc13d1341a31
DebPaine <debmalyapaine@gmail.com>
2023-08-01 02:14:05 +0530

    add: sqs resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,6 +10,11 @@ resource "random_pet" "lambda_bucket_name" {
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
+# Name of the S3 bucket has to be globally unique, hence we are generating a random name
   bucket = random_pet.lambda_bucket_name.id
 }
 
+resource "aws_sqs_queue" "message_queue" {
+# Name of the SQS doesn't have to be unique globally
+  name = "deb-terraform-sqs"
+}
