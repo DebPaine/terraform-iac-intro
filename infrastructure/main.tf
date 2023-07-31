f6be3b02f8a9267bab46245a976c963f06855214
DebPaine <debmalyapaine@gmail.com>
2023-08-01 01:42:23 +0530

    add: s3 bucket resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,11 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "random_pet" "lambda_bucket_name" {
-  prefix = "dreamsofcode"
+  prefix = "deb-terraform"
   length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
 }
 
